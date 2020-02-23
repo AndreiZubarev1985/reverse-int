b2d3a36c08164ea8249f5a53c32287980f29789f
Andrei <andreizubarev1985@gmail.com>
2020-02-24 00:18:55 +0300

    feat: my final code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n > 0) {
+        let value = String(n);
+        let result = value.split('').reverse().join(''); 
+        return +result;
+    } else {
+        let value = String(n);
+        let res = value.split('');
+        res.splice(0, 1);
+        let finalResult = res.reverse().join('');
+        return +finalResult;
+    }
+    
 }
